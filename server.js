6cfd813be497f4c48a924f15aec5c29b48864678
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-28 11:35:07 -0500

    added express middleware and a maintenance mode which is commented out until used

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express'),
-      hbs = require('hbs');
+      hbs = require('hbs'),
+      fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+
+// Uncomment this function to go into maintenance mode on the site.
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -46,5 +68,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(3333, () => {
-  console.log('Starting up zie server on port 3333');
+  console.log('Zie Server is up on port 3333');
 });

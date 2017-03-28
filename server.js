03439c89a7931e53e4e7330d6f7dd778d63b78d2
jefedeltejas <jeff.jackson@dashiell.com>
2017-03-28 09:03:46 -0500

    added handlebars partial for page title and footers

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const express = require('express'),
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
-    pageTitle: 'Home page',
+    pageTitle: 'Zie frickin homez page',
     welcomeMessage: 'Welcome Valued Express.js Customer',
     currentYear: new Date().getFullYear()
   });
